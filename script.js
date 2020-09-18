2e225bd44e3fd99f01b012804247b2cefb278df4
Helmi Priambodo <virtuoso.helmi@gmail.com>
2020-09-18 09:59:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+    this.querySelectorAll('.layer').forEach(Layer => {
+        const speed = layer.getAttribute('data-speed')
+        const x = (window.innerWidth - e.pageX*speed)
+        const y = (window.innerHeight - e.pageY*speed)
+        layer.style.transform = `translateX(${x}px) translateY(${y}px`
+    });
+}
